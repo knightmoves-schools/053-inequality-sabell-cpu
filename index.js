9342956d9cb34321ad24621cb87b2579dba53853
sabell-cpu <sabell@anamosa.k12.ia.us>
2025-10-27 18:35:05 +0000

    inequality

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+function guessNumber(guess) {
+     // Loose equality: '25' == 25 is true, so strings with "25" will be treated as equal.
+    if (guess == 25) {
+        return 'yay';
+    } else {
+        return 'nuh uh';
+    }
+}
+function strictGuessNumber(guess) {
+  // Strict equality: only the number 25 (type number) will be equal.
+  if (guess === 25) {
+    return 'yuh you got it';
+  } else {
+    return 'nope not it';
+  }
+}
